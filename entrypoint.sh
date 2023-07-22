926a446cd7b8b965196bb7bd92adca82bcd22722
flosacca <flosacca@gmail.com>
2023-07-23 06:50:45 +0800

    Make workdir consistent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 [ -z "$PANDORA_PROXY" ] || set -- "$@" -p "$PANDORA_PROXY"
 [ -z "$PANDORA_SERVER" ] || set -- "$@" -s "$PANDORA_SERVER"
 [ -z "$PANDORA_THREADS" ] || set -- "$@" --threads "$PANDORA_THREADS"
-cd "${0%/*}/src" && exec python3 -u -m pandora_cloud.launcher "$@"
+cd "${0%/*}" && PYTHONPATH=src exec python3 -u -m pandora_cloud.launcher "$@"
